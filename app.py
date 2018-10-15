a34f673e424c12ab1f4cd8840bf15973f4356adf
Austin Tanner <austin@baltosoftware.com>
2018-10-15 12:05:11 -0500

    updated db

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost:8889/flicklist'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
